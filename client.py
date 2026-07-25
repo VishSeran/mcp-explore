6eb41875fe1ba6ebfd4a3ef05289f21bc7dbe4e1
VishSeran <vishwa.sovis@gmail.com>
2026-07-25 20:51:11 +0530

    mcp-tools updated

--- client.py
+++ client.py
@@ -15,9 +15,25 @@ async def main():
     async with stdio_client as client:
         result = await client.list_tools()
         print("number of tools: ", len(result))  
+        
         for i,tool in enumerate(result):
+            print(f"tool {i+1} name: ", tool.name)
+            print(f"\ntool {i+1} description: ", tool.description)
+            print(f"\ntool {i+1} schema: ", tool.inputSchema)
             
-            print(f"tool {i+1}: ", tool.name)
+            
+        print("\n\n============Tool Interactions============\n\n")
+        
+        response = await client.call_tool(
+            
+            "resolve-library-id", {
+                "libraryName": "fastmcp",
+                "query": "How to create an MCP server using FastMCP"
+            }
+        )
+        
+        print("Response: ", response.content[0].text)
+        
 
 
 if __name__ == "__main__":
